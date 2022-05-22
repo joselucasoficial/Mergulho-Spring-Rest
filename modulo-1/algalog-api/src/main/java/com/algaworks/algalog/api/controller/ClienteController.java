516f6ad5c67f335873a4f42c00c9b26a14b2aba5
joselucasoficial <joselucasoficial@gmail.com>
2022-05-22 13:32:50 -0300

    todos em um

--- modulo-1/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ modulo-1/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,28 +3,35 @@ package com.algaworks.algalog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("joão");
-		cliente1.setEmail("joao@gmail.com");
-		cliente1.setTelefone("21 9 1111-1111");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("maria");
-		cliente2.setEmail("maria@gmail.com");
-		cliente2.setTelefone("21 9 2222-2222");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();	
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+		return clienteRepository.findById(clienteId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
