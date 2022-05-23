138ef75f01c58a717243c18bd11313bf3188d98f
joselucasoficial <joselucasoficial@gmail.com>
2022-05-22 23:14:41 -0300

    status code

--- modulo-1/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ modulo-1/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -7,12 +7,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
@@ -40,6 +42,7 @@ public class ClienteController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
